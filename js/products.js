d09c0b0937879111e875a8aba2a3bfb2d3f8ac20
Anshul Kaushal <anshul.kaushal1997@gmail.com>
2021-04-22 18:18:25 +0530

    Defined ten product objects and added them to the products array

---
+++ js/products.js
@@ -0,0 +1,101 @@
+const productOne = {
+    name: 'journal',
+    priceInCAD: {
+        original: 29,
+        discounted: 20,
+    },
+    rating: 4,
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis exercitationem facilis voluptates labore doloremque molestiae maiores, itaque repellat, harum, excepturi cupiditate voluptas maxime. Quam excepturi aliquam dolore quidem voluptatibus? Officia.',
+}
+
+const productTwo = {
+    name: 'smart wallet',
+    priceInCAD: {
+        original: 40,
+        discounted: 35,
+    },
+    rating: 4.5,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea iusto distinctio, perferendis repudiandae ab animi tempore consequatur illum deserunt exercitationem similique repellat eaque, aliquid corrupti provident, blanditiis quaerat expedita doloremque!',
+}
+
+const productThree = {
+    name: 't-shirt',
+    priceInCAD: {
+        original: 30,
+        discounted: 28,
+    },
+    rating: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptates, harum, libero est quia ipsam blanditiis in eaque recusandae nesciunt tempore. Tenetur, veritatis! Nesciunt inventore optio numquam harum quia possimus.',
+}
+
+const productFour = {
+    name: 'smart wrist-band',
+    priceInCAD: {
+        original: 100,
+        discounted: 96,
+    },
+    rating: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptates, harum, libero est quia ipsam blanditiis in eaque recusandae nesciunt tempore. Tenetur, veritatis! Nesciunt inventore optio numquam harum quia possimus.',
+}
+
+const productFive = {
+    name: 'fountain pen',
+    priceInCAD: {
+        original: 15,
+        discounted: 14,
+    },
+    rating: 3.5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptates, harum, libero est quia ipsam blanditiis in eaque recusandae nesciunt tempore. Tenetur, veritatis! Nesciunt inventore optio numquam harum quia possimus.',
+}
+
+const productSix = {
+    name: 'pocket watch',
+    priceInCAD: {
+        original: 10,
+        discounted: 7,
+    },
+    rating: 4,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea iusto distinctio, perferendis repudiandae ab animi tempore consequatur illum deserunt exercitationem similique repellat eaque, aliquid corrupti provident, blanditiis quaerat expedita doloremque!',
+}
+
+const productSeven = {
+    name: 'wayfarer',
+    priceInCAD: {
+        original: 25,
+        discounted: null,
+    },
+    rating: 4,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea iusto distinctio, perferendis repudiandae ab animi tempore consequatur illum deserunt exercitationem similique repellat eaque, aliquid corrupti provident, blanditiis quaerat expedita doloremque!',
+}
+
+const productEight = {
+    name: 'studio headphones',
+    priceInCAD: {
+        original: 200,
+        discounted: 190,
+    },
+    rating: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea iusto distinctio, perferendis repudiandae ab animi tempore consequatur illum deserunt exercitationem similique repellat eaque, aliquid corrupti provident, blanditiis quaerat expedita doloremque!',
+}
+
+const productNine = {
+    name: 'backpack',
+    priceInCAD: {
+        original: 40,
+        discounted: 38,
+    },
+    rating: 4.5,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea iusto distinctio, perferendis repudiandae ab animi tempore consequatur illum deserunt exercitationem similique repellat eaque, aliquid corrupti provident, blanditiis quaerat expedita doloremque!',
+}
+
+const productTen = {
+    name: 'laptop sleeve',
+    priceInCAD: {
+        original: 20,
+        discounted: 15,
+    },
+    rating: 4.5,
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis exercitationem facilis voluptates labore doloremque molestiae maiores, itaque repellat, harum, excepturi cupiditate voluptas maxime. Quam excepturi aliquam dolore quidem voluptatibus? Officia.',
+}
+
+const products = [productOne, productTwo, productThree, productFour, productFive, productFive, productSix, productSeven, productEight, productNine, productTen];
